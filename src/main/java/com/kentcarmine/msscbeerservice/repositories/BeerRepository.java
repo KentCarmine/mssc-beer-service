2319bc855ef56aabc1db5515d72948ee6c8a9400
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-09-11 19:25:02 -0500

    Added BeerRepository

---
+++ src/main/java/com/kentcarmine/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.kentcarmine.msscbeerservice.repositories;
+
+import com.kentcarmine.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
