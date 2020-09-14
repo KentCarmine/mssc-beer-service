1c7db76d6c936484010a6f3cb7491488dc6e2229
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-09-14 11:12:54 -0500

    Added validation and error handling to model

--- src/main/java/com/kentcarmine/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/kentcarmine/msscbeerservice/web/model/BeerDto.java
@@ -8,21 +8,42 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 }
