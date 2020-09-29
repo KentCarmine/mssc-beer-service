499205b01c08175062d00ba798e4a394ce9b0f85
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-09-29 15:37:28 -0500

    Added JSON formatting to BeerDto class

--- src/main/java/com/kentcarmine/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/kentcarmine/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.kentcarmine.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,15 +36,18 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
 }
